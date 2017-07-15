88335126432e21a2bf8453cee37bab2d082bb083
Vijay Singh <vj2010sn@gmail.com>
2017-07-15 17:12:55 +0530

    server set

--- src/components/app.js
+++ src/components/app.js
@@ -29,7 +29,7 @@ class App extends Component {
         method: 'GET'
       })
       .then((response) => {
-        response.json();
+        return response.json();
       })
       .then((res) => {
         this.setState({auth_code: 'Bearer' + res.access_token});
