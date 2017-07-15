6d6d8e19493ad0665a01c9bf9a95aa3c06ad8653
Vijay Singh <vj2010sn@gmail.com>
2017-07-15 14:27:51 +0530

    async await and server

--- server/app.js
+++ server/app.js
@@ -133,7 +133,6 @@ app.get('/refresh_token', function(req, res) {
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
-      console.log("response", access_token);
       res.send({
         'access_token': access_token
       });
--- src/components/app.js
+++ src/components/app.js
@@ -22,24 +22,19 @@ class App extends Component {
 
   getAccessToken() {
     const REFRESH_TOKEN = 'AQCo-tf2_-1SIPcJm06vDO-tPv7FaEWQIHbYwXRSLuicEVf2Eg4UMCxD-YhJOZNoDvigr3HTdB6pqmWR3ozkIrSG3jjdTf9z8DlbjdCFTEugJVcoYNHxnhR4Xb0HwKAsc2I';
-    const URL = `http://localhost:8888/refresh_token?refresh_token=${REFRESH_TOKEN}`;
-    const AUTH_HEADER = 'Basic OWQzMTM3OTM5MWU5NDQ2YmJjNDJlYzAzZmM0ZjIxM2E6N2FlMTk1YjJmNThkNGE1NGJkYmVlZDU4MWE0NGVjNDI=';
-    const AUTH_TOKEN = 'BQABtdbCmxGD9fvlg7bInIhAizIFZsaviAvcrUQpAoIdxZq-Pd2MzgR7HrbpH2Xhz819yWhLe_g_UXkggi3LBoU41G0cJm8HFHLqaZWpyZmMqU22Qbv4imtg7Sy7m6QNb1m6Ea7U4I-DycW4hB2buL8JNIaK0X0yJZwLTYdaPF9zJoVr6_k';
+    const URL = `${window.location.href}/spotify-server/refresh_token?refresh_token=${REFRESH_TOKEN}`;
     fetch(URL, {
       method: 'GET'
     })
     .then((response) => {
       console.log(response);
       return response.json();
-    })
-    .then((response) => {
-      console.log("response => ", response);
     });
   }
 
-  searchRequest(term) {
+  async searchRequest(term) {
     const URL = `https://api.spotify.com/v1/search?q=${term}&type=track`;
-    const AUTH_TOKEN = 'Bearer BQDWrGMZaowbuKgjiYcZezP3QzUh78Yjo7Axty_Ad_k6CnWjMP3lWDMf69N__tMtYYurNl417HARaXd0oxqYlzfdS2GluB55Fv-xYFaTiENOicecR4PgImEzLtH84ydH9384mr9UF_XYeemZzjeHvY6qvqV_yqvAKGPpGBzwDy7a5H90V5o';
+    const AUTH_TOKEN = "Bearer "+ await this.getAccessToken();
 
     fetch(URL, {
       method: 'GET',
